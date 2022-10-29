6a650e98020145fbb5d3fefb02e19db0af94c28d
godis <sunrenjun@corp.netease.com>
2022-10-29 23:59:00 +0800

    feat: mobile

---
+++ components/common/Header/index.tsx
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { DiscordIcon, OpenseaIcon, TwitterIcon, VectorIcon, YoutubeIcon } from '../../svg';
+import styles from './index.module.scss';
+
+const paths = [
+    { path: '/home', name: '首页' },
+    { path: '/mint', name: 'Mint' },
+    { path: '/roadmap', name: '路线图' },
+    { path: '/gallery', name: '画廊' },
+    { path: '/404', name: '合成' },
+    { path: 'https://opensea.com', name: 'opensea', components: <OpenseaIcon /> },
+    { path: 'https://discord.com', name: 'discord', components: <DiscordIcon /> },
+    { path: 'https://twitter.com', name: 'twitter', components: <TwitterIcon /> },
+    { path: 'https://instagram.com', name: 'instagram', components: <VectorIcon /> },
+    { path: 'https://youtube.com', name: 'youtube', components: <YoutubeIcon /> },
+];
+
+export default function Header() {
+    const router = useRouter();
+    return (
+        <header className="px-8 py-5 flex justify-between items-center">
+            <h1
+                className={`font-[RuiYan] rounded ${
+                    router.pathname === '/'
+                        ? 'pl-3 pr-3 pt-1 pb-1 border border-white bg-white bg-opacity-25'
+                        : ''
+                }`}
+            >
+                <Link href="/">合成豹子</Link>
+            </h1>
+            <div className={styles.navigator}>
+                {paths.map((it) => (
+                    <div
+                        key={it.path}
+                        className={` ${router.pathname === it.path ? 'bg-[#b5b5b5]' : ''}`}
+                    >
+                        {it.components ? (
+                            <>
+                                <span className="md:hidden">{it.name}</span>
+                                <a
+                                    href={it.path}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center"
+                                >
+                                    {it.components}
+                                </a>
+                            </>
+                        ) : (
+                            <Link href={it.path}>{it.name}</Link>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </header>
+    );
+}
