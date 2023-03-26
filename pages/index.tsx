cb48ab4dc3ad162e984e1a8022701415bfd88ea7
godis <sunrenjun@corp.netease.com>
2023-03-26 21:25:08 +0800

    feat: next roadmap

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ export default function Page() {
                     <Image
                         src="/images/root/worldview.jpg"
                         layout="fill"
-                        objectFit="cover"
+                        objectFit="contain"
                         alt="worldview"
                     />
                 </div>
@@ -132,7 +132,207 @@ export default function Page() {
                         alt="schematic diagram"
                     />
                 </div>
-                {/* <h1 className="text-5xl">THE BENEFITS OF MERGE LEOPARD GOD NFT</h1> */}
+                <h1 className="text-5xl py-6">THE BENEFITS OF MERGE LEOPARD GOD NFT</h1>
+                <div className="w-[64rem] mx-auto py-4">
+                    <h2 className="flex items-center pb-4">
+                        GAIN MUCH HIGHER BONUSES: <div className="flex-1 h-px bg-[#848484] ml-8" />
+                    </h2>
+                    <div className="flex">
+                        <div className="flex-1 font-light text-left py-4 pr-8">
+                            As long as you hold the round leopard NFT or the leopard god NFT, you
+                            can participate in the monthly-held lucky draw. However, the monthly
+                            lucky draw amount of the leopard god NFT will be 4 times that of the
+                            round leopard NFT, which means that you will have a chance to get a
+                            higher bonus with the leopard god NFT than the round leopard NFT.
+                            Moreover, if you hold both the round leopard NFT and the leopard god
+                            NFT, you can participate in both lucky draws and get much more bonus
+                            benefits, so holding leopard god NFT also means broadening your bonus
+                            income sources.
+                            <div>LUCKY DRAW</div>
+                        </div>
+                        <div className="relative flex-1">
+                            <Image
+                                layout="fill"
+                                objectFit="contain"
+                                src="/images/root/etthereum.png"
+                                alt="etthereum"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="w-[64rem] mx-auto py-4">
+                    <h2 className="flex items-center pb-4">
+                        <div className="flex-1 h-px bg-[#848484] mr-8" />
+                        BE ABLE TO CLAIM THE SUPREME CROWN PRIZE:
+                    </h2>
+                    <div className="flex">
+                        <div className="relative flex-1">
+                            <Image
+                                layout="fill"
+                                objectFit="contain"
+                                src="/images/root/crown.png"
+                                alt="crown"
+                            />
+                        </div>
+                        <div className="flex-1 font-light text-left py-4 pl-8">
+                            In order to encourage users to collect and merge, we have set up a very
+                            generous prize: the Supreme Crown Prize. As long as you win the prize, a
+                            very generous bonus will be given to you. The prerequisite for receiving
+                            the Supreme Crown Prize is to collect all leopard god NFT with six
+                            supreme crowns. Each merged leopard god NFT has a randomly assigned
+                            crown. This means that no matter which round leopard NFT you use to
+                            merge, you will have the chance to merge a leopard god NFT with a
+                            supreme crown. And if you are lucky enough to receive the Supreme Crown
+                            version of leopard god NFT, you will have the opportunity to receive the
+                            Supreme Crown Prize with an extremely generous bonus.
+                            <div>SUPREME CROWN PRIZE</div>
+                        </div>
+                    </div>
+                </div>
+                <h1 className="text-5xl py-6">RULE OF MERGER</h1>
+                <p className="w-[64rem] px-[10rem] mx-auto py-4 font-light">
+                    Each round leopard NFT image consists of 3 round leopard parts in different
+                    colors and directions of top, left and right. In the round leopard NFT images,
+                    three round leopard parts with different directions of the same color can be
+                    merged into a leopard god NFT of the same color for free.
+                </p>
+                <p className="w-[64rem] px-[10rem] mx-auto py-4 font-light">
+                    Each round leopard NFT can be merged up to 3 times, and the parts that have
+                    already been merged into leopard gods in each round leopard NFT image cannot
+                    continue to be merged with other round leopard images, whereas, the unmerged
+                    parts in one can continue to merge new leopard gods with other round leopard
+                    images. After running out of the number of mergence times, each round leopard
+                    NFT will only lose its mergence function, instead of being destroyed.
+                </p>
+                <div className="relative w-[62rem] h-[30rem] mx-auto">
+                    <Image
+                        layout="fill"
+                        objectFit="contain"
+                        src="/images/root/merge.png"
+                        alt="merge"
+                    />
+                </div>
+                <h1 className="text-5xl py-6">REPEATED SYNTHESIS RULE</h1>
+                <p className="w-[64rem] px-[10rem] mx-auto py-4 font-light">
+                    In the 3 round leopard NFT images below, there are 2 sets of round leopard parts
+                    of the same color in different directions, then they can merge 2 leopard god NFT
+                    of the same color for free.
+                </p>
+                <div className="relative w-[62rem] h-[10rem] mx-auto">
+                    <Image
+                        layout="fill"
+                        objectFit="contain"
+                        src="/images/root/merge2.png"
+                        alt="merge2"
+                    />
+                </div>
+                <p className="w-[64rem] px-[10rem] mx-auto py-4 font-light">
+                    In the 3 round leopard NFT images below, there are 3 sets of round leopard parts
+                    of the same color in different directions, then they can merge 3 leopard god NFT
+                    of the same color for free.
+                </p>
+                <div className="relative w-[62rem] h-[10rem] mx-auto">
+                    <Image
+                        layout="fill"
+                        objectFit="contain"
+                        src="/images/root/merge3.png"
+                        alt="merge3"
+                    />
+                </div>
+                <h1 className="text-5xl pt-14 pb-5">BONUS SYSTEM</h1>
+                <h2 className="text-xl text-[#D7C19A] pb-8">BREAK THE SEAL OF THE LEOPARD GOD</h2>
+                <p className="w-[64rem] px-[10rem] mx-auto py-4 font-light">
+                    Unseal the Leopard God: Once you have collected all 6 Supreme crowns, you can
+                    unseal the Leopard God. and those who unseal will also get rich rewards.
+                </p>
+                <div className="relative h-[36rem]">
+                    <Image
+                        src="/images/root/supreme-crown.jpg"
+                        layout="fill"
+                        objectFit="contain"
+                        alt="supreme-crown"
+                    />
+                </div>
+                <h1 className="text-5xl pt-14 pb-5">BONUS SYSTEM COMPONENTS</h1>
+                <p className="w-[64rem] px-[10rem] mx-auto py-4 font-light">
+                    The bonus system is designed to bring more fun and benefits to community
+                    users.Our bonus system consists of two parts, one is the bonus pool system, the
+                    other is the lucky draw system.
+                </p>
+                <div className="w-[48rem] px-12 py-3 mb-6 mx-auto border border-[#D7C19A] rounded-3xl text-left">
+                    <h2 className="text-xl text-[#D7C19A] pb-4">BONUS POOL SYSTEM</h2>
+                    <p className="font-light">
+                        The bonus system is designed to bring more fun and benefits to community
+                        users.Our bonus system consists of two parts, one is the bonus pool system,
+                        the other is the lucky draw system.
+                    </p>
+                </div>
+                <div className="w-[48rem] px-12 py-3 mb-6 mx-auto border border-[#D7C19A] rounded-3xl text-left">
+                    <h2 className="text-xl text-[#D7C19A] pb-4">LUCKY DRAW SYSTEM</h2>
+                    <p className="font-light">
+                        The lucky draw system is designed to promote user transactions. In the
+                        system, we have designed three kinds of awards, namely, the monthly lucky
+                        draw of round leopard NFT and leopard god NFT and the Supreme Crown Award.
+                        The bonuses of these awards come from the funds accumulated by the bonus
+                        pool system. Since the bonus pool funds are made up of 20% mint fees and 4%
+                        of each transaction fee in the secondary market, it means that the more mint
+                        users and secondary market transactions, the higher the transaction fees, so
+                        the more bonuses can be accumulated in the bonus pool, and then the more
+                        bonuses the winners will get.
+                    </p>
+                </div>
+                <div className="w-[48rem] px-12 py-3 mb-6 mx-auto border border-[#D7C19A] rounded-3xl text-left">
+                    <h2 className="text-xl text-[#D7C19A] pb-4">MONTHLY ROUND LEOPARD AWARD</h2>
+                    <p className="font-light">
+                        As long as you own the round leopard NFT, you will be able to participate in
+                        the lucky draw for round leopard NFT users four times a month on the 6th,
+                        13th, 20th and 27th of each month. with the bonus accounting for 5% of the
+                        total amount of the current bonus pool. And the unconsumed bonus in the
+                        current month will be automatically accumulated into the prize pool of the
+                        next month.
+                    </p>
+                </div>
+                <div className="w-[48rem] px-12 py-3 mb-6 mx-auto border border-[#D7C19A] rounded-3xl text-left">
+                    <h2 className="text-xl text-[#D7C19A] pb-4">MONTHLY LEOPARD GOD AWARD</h2>
+                    <p className="font-light">
+                        If you hold a leopard god NFT, you will be able to participate in a monthly
+                        lucky draw for leopard god NFT users on the 27th monthly. with the bonus
+                        accounting for 20% of the total amount of the current bonus pool. And the
+                        unconsumed bonus in the current month will be automatically accumulated into
+                        the prize pool of the next month.
+                    </p>
+                </div>
+                <div className="w-[48rem] px-12 py-3 mb-6 mx-auto border border-[#D7C19A] rounded-3xl text-left">
+                    <h2 className="text-xl text-[#D7C19A] pb-4">SUPREME CROWN AWARD</h2>
+                    <p className="font-light">
+                        If you collect all leopard god NFT with six supreme crowns, then you can
+                        receive the Crown Award, which can be received only once a month, on the
+                        27th of each month. There will be one winner each time, with the bonus
+                        accounting for 60% of the total amount of the current bonus pool. If no one
+                        receives it in the current month, the bonus will be automatically
+                        accumulated into the bonus pool of the next month.
+                    </p>
+                </div>
+                <h1 className="text-5xl pt-14 pb-5">FLOW CHART OF BONUS DISTRIBUTION</h1>
+                <p className="w-[64rem] px-[10rem] mx-auto py-4 font-light">
+                    Attention: The supreme crown bonuses and monthly lucky draw bonuses come from
+                    not only the accumulated funds--automatically deducted 4% ETH of transaction
+                    fees for each transaction in the secondary market, but NFT holders can also
+                    inject more money into the bonus pool at any time to increase the bonus chips.
+                </p>
+                <div className="bg-[#D7C19A] p-1 w-96 mx-auto rounded-sm text-black my-4 text-xs">
+                    INJECT MORE WINNINGS INTO THE BONUS POOL
+                </div>
+                <div className="w-[64rem] px-8 mx-auto border border-[#D7C19A] rounded-3xl">
+                    <div className="relative w-[60rem] h-72">
+                        <Image
+                            src="/images/root/prize-pool.png"
+                            layout="fill"
+                            objectFit="contain"
+                            alt="prize pool"
+                        />
+                    </div>
+                </div>
             </div>
         </Layout>
     );
