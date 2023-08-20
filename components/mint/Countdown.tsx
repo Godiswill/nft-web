df1b0f0020b601c54f293ba91721ba5a0b4defd2
godis <sunrenjun@corp.netease.com>
2023-08-20 11:26:47 +0800

    feat: css margin

--- components/mint/Countdown.tsx
+++ components/mint/Countdown.tsx
@@ -8,13 +8,13 @@ function Countdown() {
     return (
         <>
             <h2>Coming soon, stay tuned</h2>
-            <div className="my-6 text-lg">
+            <div className="my-8 text-lg">
                 <div className="mb-4">Aug 25, 2023</div>
                 <div className="mb-4">600 supply (Max 2/wallet)</div>
                 <div className="mb-4">Maximum 10 NFTs per wallet!</div>
             </div>
 
-            <div className="flex justify-around text-4xl text-center mt-14 mb-10">
+            <div className="flex justify-around text-4xl text-center mt-20 mb-14">
                 <div>
                     <div>7</div>
                     <div className="mt-2 text-2xl">DAYS</div>
@@ -33,41 +33,43 @@ function Countdown() {
                 </div>
             </div>
 
-            {wlInfo ? (
-                <div className="mb-4 bg-white/[0.15] text-white text-left mx-auto lg:w-96 p-8 rounded relative">
-                    Please follow our official{' '}
-                    <a
-                        className="text-[#D7C19A]"
-                        href="https://twitter.com/0xleopardgod"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Twitter
-                    </a>{' '}
-                    and join our official{' '}
-                    <a
-                        className="text-[#D7C19A]"
-                        href="https://discord.gg/GdPzmcpxQ2"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Discord
-                    </a>{' '}
-                    community to get whitelist event information.
-                    <div
-                        className="absolute top-2 right-2 cursor-pointer"
-                        onClick={() => setWLInfo(false)}
-                    >
-                        <CloseIcon2 />
+            <div className="mb-10">
+                {wlInfo ? (
+                    <div className="mb-4 bg-white/[0.15] text-white text-left mx-auto lg:w-96 p-8 rounded relative">
+                        Please follow our official{' '}
+                        <a
+                            className="text-[#D7C19A]"
+                            href="https://twitter.com/0xleopardgod"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Twitter
+                        </a>{' '}
+                        and join our official{' '}
+                        <a
+                            className="text-[#D7C19A]"
+                            href="https://discord.gg/GdPzmcpxQ2"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Discord
+                        </a>{' '}
+                        community to get whitelist event information.
+                        <div
+                            className="absolute top-2 right-2 cursor-pointer"
+                            onClick={() => setWLInfo(false)}
+                        >
+                            <CloseIcon2 />
+                        </div>
                     </div>
-                </div>
-            ) : (
-                <InfoTip
-                    title="How to Secure Your Whitelist Spot?"
-                    type="info"
-                    onClick={() => setWLInfo(true)}
-                />
-            )}
+                ) : (
+                    <InfoTip
+                        title="How to Secure Your Whitelist Spot?"
+                        type="info"
+                        onClick={() => setWLInfo(true)}
+                    />
+                )}
+            </div>
         </>
     );
 }
