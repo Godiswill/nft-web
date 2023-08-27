9c8288e1b102d39dc4885888b2528bf95e94287b
godis <sunrenjun@corp.netease.com>
2023-08-27 16:55:22 +0800

    feat: rm minted

--- contract/merkletree.ts
+++ contract/merkletree.ts
@@ -2,13 +2,21 @@ import { MerkleTree } from 'merkletreejs';
 import keccak256 from 'keccak256';
 import wl from './wl.json';
 
-const whitelist = Array.from(new Set(wl.free));
+const free = Array.from(new Set(wl.free));
+const minted = Array.from(new Set(wl.minted || []));
+const whitelist = free.filter((it) => !minted.includes(it));
+
+// console.log(wl.free.length, free.length, wl.minted?.length, minted.length, whitelist.length);
+// console.log(whitelist);
+
+// console.log('whitelist----', whitelist.length === wl.free.length);
+// console.log(JSON.stringify({ whitelist }));
 
 const leafNodes = whitelist.map((addr) => keccak256(addr));
 const merkletree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
 
 const rootHash = merkletree.getHexRoot();
-
+// console.log(rootHash);
 const getProof = (addr: `0x${string}` | undefined) => {
     if (!addr) return [];
     return merkletree.getHexProof(keccak256(addr));
